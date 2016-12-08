946fbb418c8a59951348c96f4362ad8f33e8e9b1
Nicl7004 <nicholas.clement@colorado.edu>
2016-12-07 23:43:00 -0700

    working on travis and pip

--- tests/module_tests.py
+++ tests/module_tests.py
@@ -1,6 +1,11 @@
 
 import sys
-sys.path.append('../..') #set path to recognize new twitterToy package
+import os
+sys.path.append(str(os.path.dirname(os.path.abspath("module_tests.py"))).replace("/tests",""))
+print(str(os.path.dirname(os.path.abspath("module_tests.py"))).replace("/tests",""),"\n\n")
+# sys.path.append('~/twitterToy') #set path to recognize new twitterToy package
+# sys.path.append("../..")
+print (sys.path)
 import twitterToy.modules.user as user
 def main():
     y = user.user()
