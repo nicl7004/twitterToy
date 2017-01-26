fbc3426ecd53deb530c508c39e09a83f66105afd
Nicl7004 <nicholas.clement@colorado.edu>
2017-01-26 12:45:53 -0700

    extended tests for network visualizations

--- tests/extended_tests.py
+++ tests/extended_tests.py
@@ -9,12 +9,14 @@ if sys.version_info[0] < 3: #for python 2.7 and under
     (sys.path.append(str(os.path.abspath('..')) + "/twitterToy/"))
     from modules import user
     from modules import followers_tweets_scatter
+    from modules import network
 
 else:                       #for python 3 and up
     sys.path.insert(0, os.path.abspath('..'))
     sys.path.append('../..')
     from twitterToy.modules import user
     from twitterToy.modules import followers_tweets_scatter
+    from twitterToy.modules import network
 
 
 class SimplisticTest(unittest.TestCase):
@@ -26,6 +28,10 @@ class SimplisticTest(unittest.TestCase):
     def testFollowersTweetsScatter(self):
         followers_tweets_scatter.followers_tweets_scatter()
 
+    def testNetwork(self):
+        x = network.network()
+        x.draw()
+        x.networkSize()
 
 
 
