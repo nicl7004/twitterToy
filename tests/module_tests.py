fed463694125cf38521e529d8c5c6b48d217dd86
Nicl7004 <nicholas.clement@colorado.edu>
2017-01-26 23:00:01 -0700

    email and additional tests

--- tests/module_tests.py
+++ tests/module_tests.py
@@ -7,13 +7,13 @@ import unittest
 
 if sys.version_info[0] < 3: #for python 2.7 and under
     (sys.path.append(str(os.path.abspath('..')) + "/twitterToy/"))
-    from modules import user, followingVsFollwers, followers_tweets_scatter
+    from modules import user, followingVsFollwers, followers_tweets_scatter, globalTrends
     from database import databaseHelper
 
 else:                       #for python 3 and up
     sys.path.insert(0, os.path.abspath('..'))
     sys.path.append('../..')
-    from twitterToy.modules import user, followingVsFollwers, followers_tweets_scatter
+    from twitterToy.modules import user, followingVsFollwers, followers_tweets_scatter, globalTrends
     from twitterToy.database import databaseHelper
 
 class SimplisticTest(unittest.TestCase):
@@ -32,6 +32,22 @@ class SimplisticTest(unittest.TestCase):
     #database tests
     def testEmailList(self):
         databaseHelper.getEmailList()
+
+    def testTrend(self):
+        trend = globalTrends.trends()
+        trend.getGlobalTrends()
+
+    def testTrendWash(self):
+        x = globalTrends.trends()
+        unwashed = x.getGlobalTrends()
+        washed = x.washTrend(unwashed)
+
+    def testTrendEmail(self):
+        x = globalTrends.trends()
+        unwashed = x.getGlobalTrends()
+        washed = x.washTrend(unwashed)
+        x.sendEmail("nicholas.clement@colorado.edu", "Twitter Trending Update", washed)
+
 
 
 
